a637376889fad18b6b657ef260d98ab132a3c41c
Chimas <50328145+igor-saadeh@users.noreply.github.com>
2024-10-19 15:48:09 -0300

    Ajuste Visual - Golem_Spawner

--- GameJamProject_2024/Assets/Scripts/Golem_Spawner.cs
+++ GameJamProject_2024/Assets/Scripts/Golem_Spawner.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class Golem_Spawner : MonoBehaviour
 {
     private GameObject golem;
+    private Color initialColor;
+    private Renderer myRenderer;
 
     void Start()
     {
         golem = Resources.Load<GameObject>("Prefabs/Golem");
+        myRenderer = GetComponent<Renderer>();
+        initialColor = myRenderer.material.color;
     }
 
     private void OnMouseDown()
     {
         Vector3 offset = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
         Instantiate(golem, offset, Quaternion.identity);
+    }
+
+    private void OnMouseOver()
+    {
+        myRenderer.material.SetColor("_Color", Color.green);
+    }
+
+    private void OnMouseExit()
+    {
+        myRenderer.material.SetColor("_Color", initialColor);
     }
 }
