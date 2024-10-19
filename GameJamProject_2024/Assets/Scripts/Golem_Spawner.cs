7585b933cdfd82e76a8dbe0160fd1da35f2ae6ba
Igor Gilberto Soares <igorgilbertosoares@gmail.com>
2024-10-19 20:09:19 -0300

    golem spawn, stats and places

--- GameJamProject_2024/Assets/Scripts/Golem_Spawner.cs
+++ GameJamProject_2024/Assets/Scripts/Golem_Spawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Golem_Spawner : MonoBehaviour
 {
-    private GameObject golem;
+    [SerializeField] GameObject golem;
     private Color initialColor;
     private Renderer myRenderer;
 
